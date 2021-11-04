7115632b1d1d739c48521375195afed8e483ee4d
Jeremy Rubin <j@rubin.io>
2021-11-04 03:30:12 -0400

    Add Configured By Environment Var

--- runner.sh
+++ runner.sh
@@ -10,6 +10,7 @@ export SAPIO_BITCOIN_NETWORK="regtest"
 export SAPIO_ORACLE_SEED_FILE='/home/app/ORACLE_SEED'
 export SAPIO_ORACLE_NET=0.0.0.0:8010
 export SAPIO_CLI_BINARY='/home/app/sapio-cli'
+export SAPIO_CLI_CONFIGURED_BY='here'
 
 chown -R ubitcoin:ubitcoin /home/ubitcoin/.bitcoin
 su ubitcoin -c "cd && ./rpcauth.py ubitcoin $SAPIO_BITCOIN_RPC_PASSWORD | head -n2 | tail -n 1 >> .bitcoin/bitcoin.conf"
